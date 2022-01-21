8fcb6a2b71a59a95fae709a8ab6e0473f240a8b7
tusmester <tusmester@gmail.com>
2022-01-21 22:10:35 +0100

    Add initial web app.

---
+++ src/LakossagStat.WebApp/Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LakossagStat.Data.Loaders;
+using Microsoft.Extensions.Logging;
+
+namespace LakossagStat.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            
+            //UNDONE: configure logging
+            services.AddLogging(builder => builder.AddConsole());
+
+            services.Configure<DataLoaderOptions>(options =>
+            {
+                options.Path = "https://www.valasztas.hu/telepulesek-lakossag-es-valasztopolgarszama";
+            });
+            services.AddSingleton<IDataLoader, HtmlDataLoader>();
+            services.AddHostedService<TimedLoaderService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
