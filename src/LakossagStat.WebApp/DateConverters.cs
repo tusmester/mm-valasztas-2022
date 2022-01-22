cc3f6c83957a75c6e9d756a325b90ec64480fab6
tusmester <tusmester@gmail.com>
2022-01-22 20:42:12 +0100

    Show refresh date in ceu time zone.

---
+++ src/LakossagStat.WebApp/DateConverters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TimeZoneInfo = System.TimeZoneInfo;
+
+namespace LakossagStat.WebApp
+{
+    public static class DateConverters
+    {
+        private static readonly TimeZoneInfo TziHu = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+
+        public static DateTime ToCentralEuropean(this DateTime utcDate)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(utcDate, TziHu);
+        }
+    }
+}
