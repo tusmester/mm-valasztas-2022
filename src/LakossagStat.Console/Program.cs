886b2b68471d85c408068ec00f60ce8540a96655
tusmester <tusmester@gmail.com>
2022-01-26 13:42:42 +0100

    Refactor console app to use services and save json locally.

--- src/LakossagStat.Console/Program.cs
+++ src/LakossagStat.Console/Program.cs
@@ -1,35 +1,60 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using LakossagStat.Data;
 using LakossagStat.Data.Loaders;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
 namespace LakossagStat.Console
 {
     class Program
     {
+        const string ParentPath = "./App_Data";
+        const string FilePath = ParentPath + "/lakossag-data.json";
+
         static async Task Main(string[] args)
         {
-            //var path = "./lakossag.xls";
-            var path = "https://www.valasztas.hu/telepulesek-lakossag-es-valasztopolgarszama";
-            var options = new DataLoaderOptions
-            {
-                Path = path,
-                OverwriteLocalFile = true
-            };
+            using var host = CreateHostBuilder(args).Build();
 
-            //var loader = new FileDataLoader(Options.Create(options), NullLoggerFactory.Instance.CreateLogger<FileDataLoader>());
-            //var loader = new UrlDataLoader(Options.Create(options), NullLoggerFactory.Instance.CreateLogger<UrlDataLoader>());
-            //var loader = new HtmlDataLoader(Options.Create(options), NullLoggerFactory.Instance.CreateLogger<HtmlDataLoader>());
+            var logger = host.Services.GetService<ILogger<Program>>();
+            var loader = host.Services.GetService<IDataLoader>();
 
-            //var data = await loader.LoadAsync();
-            //var analyzer = new DataAnalyzer(data);
+            logger.LogInformation($"Loading data...");
 
-            //analyzer.Analyze();
+            var data = await loader.LoadAsync();
 
-            //var json = JsonConvert.SerializeObject(data);
+            logger.LogInformation($"Analyzing data...");
+
+            var analyzer = new DataAnalyzer(data);
+
+            analyzer.Analyze();
+
+            logger.LogInformation($"Analyzing finished.");
+
+            var json = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            if (!Directory.Exists(ParentPath))
+                Directory.CreateDirectory(ParentPath);
+
+            File.WriteAllText(FilePath, json, Encoding.UTF8);
+
+            logger.LogInformation($"Data is saved to {FilePath}.");
         }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hb, services) => services
+                    .AddLogging(logging =>
+                    {
+                        logging.AddConsole();
+                        logging.AddFile("Logs/mm-voksturizmus-{Date}.txt", LogLevel.Trace);
+                    })
+                    .AddHttpClient()
+                    // configure feature-specific options
+                    .Configure<DataLoaderOptions>(hb.Configuration.GetSection("DataLoader"))
+                    .AddSingleton<IDataLoader, HtmlDataLoader>());
     }
 }
